09016e3ce01f4c749a6b47c0d37424638cfbf48e
Terata Yuri <y.terata@a-force.co.jp>
2022-04-27 10:27:38 +0900

    インデントをそろえる

--- js/intro.js
+++ js/intro.js
@@ -12,8 +12,8 @@ let bar = new ProgressBar.Line(splash_text, {//id名を指定
 			top: '50%',
 			padding: '0',
 			margin: '-30px 0 0 0',//バーより上に配置
-			transform:'translate(-50%,-50%)',
-			'font-size':'1rem',
+			transform: 'translate(-50%,-50%)',
+			'font-size': '1rem',
 			color: '#fff',
 		},
 		autoStyleContainer: false //自動付与のスタイルを切る
@@ -24,50 +24,48 @@ let bar = new ProgressBar.Line(splash_text, {//id名を指定
 });
 
 //アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+bar.animate(1.0, function() {//バーを描画する割合を指定します 1.0 なら100%まで描画します
 	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
 });
 
 
 $('.slider').slick({
-		fade:true,//切り替えをフェードで行う。初期値はfalse。
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		autoplaySpeed: 5000,//次のスライドに切り替わる待ち時間
-		speed:1000,//スライドの動きのスピード。初期値は300。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 1,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-		arrows: true,//左右の矢印あり
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		dots: true,//下部ドットナビゲーションの表示
-        pauseOnFocus: false,//フォーカスで一時停止を無効
-        pauseOnHover: false,//マウスホバーで一時停止を無効
-        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+	fade: true,//切り替えをフェードで行う。初期値はfalse。
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 5000,//次のスライドに切り替わる待ち時間
+	speed: 1000,//スライドの動きのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 1,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+	pauseOnFocus: false,//フォーカスで一時停止を無効
+	pauseOnHover: false,//マウスホバーで一時停止を無効
+	pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
 });
 
 //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.slider').slick('slickPlay');
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide) {
+	$('.slider').slick('slickPlay');
 });
 
 
-$(".openbtn").click(function () {//ボタンがクリックされたら
+$(".openbtn").click(function() {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+	$("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
 });
 
-$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+$("#g-nav a").click(function() {//ナビゲーションのリンクがクリックされたら
+	$(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+	$("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
 });
 
 
-$a[href*="#"].click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+$a[href *= "#"].click(function() {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
 	let elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
 	let pos = $(elmHash).offset().top;	//idの上部の距離を取得
-	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	$('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
-
-
